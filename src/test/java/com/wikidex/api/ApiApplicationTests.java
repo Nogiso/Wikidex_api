704323ed8a4d7411da4a04e45168e583859a8f71
Nagisa <lesaulnier.mat@gmail.com>
2022-09-07 15:55:48 +0200

    Ajout projet Spring Boot

---
+++ src/test/java/com/wikidex/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikidex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
